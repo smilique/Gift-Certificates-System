5ab0a3a2ac0bdb644ddd45b054b1ed1fb5534209
smilique <smile.nvp@gmail.com>
2021-09-15 09:25:08 +0300

    dbcp pool

--- src/main/java/com/epam/esm/config/JdbcConfig.java
+++ src/main/java/com/epam/esm/config/JdbcConfig.java
@@ -1,12 +1,11 @@
 package com.epam.esm.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -22,14 +21,23 @@ public class JdbcConfig {
     private String username;
     @Value("${spring.datasource.password}")
     private String password;
+    @Value("${spring.datasource.max-idle}")
+    private int maxIdle;
+    @Value("${spring.datasource.max-total}")
+    private int maxTotal;
+    @Value("${spring.datasource.max-wait-time}")
+    private Long maxWaitTime;
 
     @Bean
     public DataSource createDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
+        dataSource.setMaxIdle(maxIdle);
+        dataSource.setMaxTotal(maxTotal);
+        dataSource.setMaxWaitMillis(maxWaitTime);
         return dataSource;
     }
 
