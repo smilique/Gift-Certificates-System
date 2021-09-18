49cc577f59fd830f948774bea4d4e4513464dad5
smilique <smile.nvp@gmail.com>
2021-09-19 02:55:45 +0300

    config update

--- src/main/java/com/epam/esm/config/JdbcConfig.java
+++ src/main/java/com/epam/esm/config/JdbcConfig.java
@@ -13,19 +13,27 @@ import javax.sql.DataSource;
 @PropertySource("classpath:application.properties")
 public class JdbcConfig {
 
-    @Value("${spring.datasource.driver-class-name}")
+    private static final String DRIVER_CLASSNAME = "${spring.datasource.driver-class-name}";
+    private static final String URL = "${spring.datasource.url}";
+    private static final String USERNAME = "${spring.datasource.username}";
+    private static final String PASSWORD = "${spring.datasource.password}";
+    private static final String MAX_IDLE = "${spring.datasource.max-idle}";
+    private static final String MAX_TOTAL = "${spring.datasource.max-total}";
+    private static final String MAX_WAIT_TIME = "${spring.datasource.max-wait-time}";
+
+    @Value(DRIVER_CLASSNAME)
     private String driverClassName;
-    @Value("${spring.datasource.url}")
+    @Value(URL)
     private String url;
-    @Value("${spring.datasource.username}")
+    @Value(USERNAME)
     private String username;
-    @Value("${spring.datasource.password}")
+    @Value(PASSWORD)
     private String password;
-    @Value("${spring.datasource.max-idle}")
+    @Value(MAX_IDLE)
     private int maxIdle;
-    @Value("${spring.datasource.max-total}")
+    @Value(MAX_TOTAL)
     private int maxTotal;
-    @Value("${spring.datasource.max-wait-time}")
+    @Value(MAX_WAIT_TIME)
     private Long maxWaitTime;
 
     @Bean
--- src/main/java/com/epam/esm/config/WebConfig.java
+++ src/main/java/com/epam/esm/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.epam.esm.config;
 
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -10,7 +12,6 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.epam.esm")
-@PropertySource({"classpath:application.properties"})
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
@@ -19,7 +20,6 @@ public class WebConfig implements WebMvcConfigurer {
         resolver.setPrefix("/WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
         resolver.setViewClass(JstlView.class);
-
         return resolver;
     }
 }
