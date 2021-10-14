9a835ffe6424a14c55808fa5c479668cb2ae7abc
smilique <smile.nvp@gmail.com>
2021-10-14 11:29:54 +0300

    spring boot project configuration

--- configuration/src/main/java/com/epam/esm/configuration/SpringMvcApplicationInitializer.java
+++ configuration/src/main/java/com/epam/esm/configuration/SpringMvcApplicationInitializer.java
@@ -7,19 +7,17 @@ import javax.servlet.ServletException;
 
 public class SpringMvcApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    private static final String PROFILE_PARAMETER = "spring.profiles.active";
-    private static final String CURRENT_PROFILE = "dev";
     private static final String THROW_EXCEPTION_PARAMETER = "throwExceptionIfNoHandlerFound";
     private static final String THROW_EXCEPTIONS = "true";
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[]{DevJdbcConfig.class, ProdJdbcConfig.class};
+        return new Class[]{};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{WebConfig.class};
+        return new Class[]{};
     }
 
     @Override
@@ -29,10 +27,7 @@ public class SpringMvcApplicationInitializer extends AbstractAnnotationConfigDis
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        servletContext.setInitParameter(PROFILE_PARAMETER, CURRENT_PROFILE);
         servletContext.setInitParameter(THROW_EXCEPTION_PARAMETER, THROW_EXCEPTIONS);
-        servletContext.setInitParameter("server.error.whitelabel.enabled", "false");
-        servletContext.setInitParameter("spring.mvc.throw-exception-if-no-handler-found","true");
         super.onStartup(servletContext);
     }
 }
